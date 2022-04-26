8b0986a82d6e00faf271f8d911bd81b635622ee4
Zineb-Belbhiriya <zineb.belbhiriya012@gmail.com>
2022-04-26 11:33:49 +0000

    tailwind style initialization &Vuex &Vue Router

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createBlog } from "vuex";
+
+export default createBlog({
+  state: {
+    firstName: "zineb",
+    lastName: "belbhiriya",
+  },
+  mutations: {},
+  actions: {},
+  getters: {},
+});
